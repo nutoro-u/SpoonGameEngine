63b709258dc50bb7f66f3fc932a32af8a1b632e6
mezanix <mezanixissa@gmail.com>
2025-12-27 10:17:48 +0200

    project templates generated

--- SpoonEditor/GameProject/NewProject.cs
+++ SpoonEditor/GameProject/NewProject.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿using SpoonEditor.Utils;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
 
 namespace SpoonEditor.GameProject
 {
-    class NewProject : ViewModelBase
-    {
+	[DataContract]
+	public class ProjectTemplate
+	{
+		[DataMember]
+		public string ProjectType { get; set; }
+		
+		[DataMember]
+		public string ProjectFile { get; set; }
+		
+		[DataMember]
+		public List<string> Folders { get; set; }
+	}
+
+	class NewProject : ViewModelBase
+	{
+		// TODO get the path from the installation location
+		private string _templatesPath = @"..\..\SpoonEditor\ProjectTemplates";
+
 		private string _name = "NewProject";
-		public string Name 
+		public string Name
 		{
 			get => _name;
 			set
@@ -31,6 +51,31 @@ namespace SpoonEditor.GameProject
 					_path = value;
 					OnPropertyChanged(nameof(Path));
 				}
+			}
+		}
+
+		public NewProject()
+		{
+			try
+			{
+				string[] templateFiles = Directory.GetFiles(_templatesPath, "template.xml", SearchOption.AllDirectories);
+				Debug.Assert(templateFiles.Any());
+				foreach (string file in templateFiles)
+				{
+					ProjectTemplate template = new ProjectTemplate()
+					{
+						ProjectType = "Empty Project",
+						ProjectFile = "Project.spoonengine",
+						Folders = new List<string> { ".Spoon", "Content", "GameCode"},
+					};
+
+					Serializer.ToFile(template, file);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+				// TODO log error
 			}
 		}
 	}

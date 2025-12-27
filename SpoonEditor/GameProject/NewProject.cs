3d36521d80935fe80f78115f54fb4f384dc5399e
mezanix <mezanixissa@gmail.com>
2025-12-27 06:31:04 +0200

    new project model & project templates images

---
+++ SpoonEditor/GameProject/NewProject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpoonEditor.GameProject
+{
+    class NewProject : ViewModelBase
+    {
+		private string _name = "NewProject";
+		public string Name 
+		{
+			get => _name;
+			set
+			{
+				if (_name != value)
+				{
+					_name = value;
+					OnPropertyChanged(nameof(Name));
+				}
+			}
+		}
+
+		private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\SpoonProjects\";
+		public string Path
+		{
+			get => _path;
+			set
+			{
+				if (_path != value)
+				{
+					_path = value;
+					OnPropertyChanged(nameof(Path));
+				}
+			}
+		}
+	}
+}

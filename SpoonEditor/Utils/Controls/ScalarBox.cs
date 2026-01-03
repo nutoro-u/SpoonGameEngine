ebd75c1aa8e24bbc2b485bd91d13eba357ec83d4
mezanix <mezanixissa@gmail.com>
2026-01-03 07:56:12 +0200

    numberbox

---
+++ SpoonEditor/Utils/Controls/ScalarBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace SpoonEditor.Utils.Controls
+{
+	class ScalarBox : NumberBox
+	{
+		static ScalarBox()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(ScalarBox),
+				new FrameworkPropertyMetadata(typeof(ScalarBox)));
+		}
+	}
+}

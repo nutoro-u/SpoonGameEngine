3520e4e92c58e2af7b6590816e4c9757d578fe8e
mezanix <mezanixissa@gmail.com>
2026-01-06 17:41:53 +0200

    Generate uv sphere

--- SpoonEditor/DllWrappers/ContentToolsAPI.cs
+++ SpoonEditor/DllWrappers/ContentToolsAPI.cs
@@ -19,6 +19,20 @@ namespace SpoonEditor.ContentToolsAPIStructs
 		public byte ReverseHandedness = 0;
 		public byte ImportEmbededTextures = 1;
 		public byte ImportAnimations = 1;
+
+		private byte ToByte(bool value) => value ? (byte)1 : (byte)0;
+
+		public void FromContentSettings(Content.Geometry geometry)
+		{
+			var settings = geometry.ImportSettings;
+
+			SmoothingAngle = settings.SmootingAngle;
+			CalculateNormals = ToByte(settings.CalculateNormals);
+			CalculateTangents = ToByte(settings.CalculateTangents);
+			ReverseHandedness = ToByte(settings.ReverseHandedness);
+			ImportEmbededTextures = ToByte(settings.ImportEmbeddedTextures);
+			ImportAnimations = ToByte(settings.ImportAnimations);
+		}
 	}
 
 	[StructLayout(LayoutKind.Sequential)]
@@ -66,6 +80,7 @@ namespace SpoonEditor.DllWrappers
 			using var sceneData = new SceneData();
 			try
 			{
+				sceneData.ImportSettings.FromContentSettings(geometry);
 				CreatePrimitiveMesh(sceneData, info);
 				Debug.Assert(sceneData.Data != IntPtr.Zero && sceneData.DataSize > 0);
 				var data = new byte[sceneData.DataSize];
